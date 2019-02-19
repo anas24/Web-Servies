3a896fd652c20b4fc36e889a96f7060c5131df73
anas24 <qazianas2412@gmail.com>
2019-02-19 18:15:44 +0530

    Pagination implemented

--- mvc-rest/controller/userController.go
+++ mvc-rest/controller/userController.go
@@ -3,109 +3,42 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
-	"mvc-rest/database"
 	_ "mvc-rest/database"
 	"mvc-rest/model"
 	"net/http"
 )
 
-var users []model.User
-
 func AllUsers(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(database.Db)
-
-	allUsers, err := database.Db.Query("SELECT id, name FROM users")
-	defer allUsers.Close()
-	defer database.Db.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for allUsers.Next() {
-		var id int
-		var name string
-		err := allUsers.Scan(&id, &name)
-		if err != nil {
-			panic(err)
-		} else {
-			fmt.Println(id, "  ", name)
-		}
-		user := model.User{id, name}
-		users = append(users, user)
-	}
-	fmt.Println("EndPoint hit: All articles endpoint", allUsers, err)
-	json.NewEncoder(w).Encode(users)
+	json.NewEncoder(w).Encode(model.AllUsers(w, r))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page endPoinHit")
 }
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	nId := r.URL.Query().Get("id")
-	user, err := database.Db.Query("SELECT id, name FROM users where id = ?", nId)
-	defer user.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
-	if user != nil {
-		for user.Next() {
-			var id int
-			var name string
-			err := user.Scan(&id, &name)
-			if err != nil {
-				panic(err.Error())
-			} else {
-				userData := model.User{id, name}
-				fmt.Println(id, name)
-				json.NewEncoder(w).Encode(userData)
-			}
-		}
-	}
+	json.NewEncoder(w).Encode(model.GetUser(w, r))
 }
 
 func Insert(w http.ResponseWriter, r *http.Request) {
-	nId := r.URL.Query().Get("id")
-	name := r.URL.Query().Get("name")
-	newUser, err := database.Db.Prepare("Insert into users(id, name) values(?, ?)")
-	defer newUser.Close()
-	if err != nil {
-		fmt.Println(err)
-		fmt.Fprint(w, "Params Invalid")
-	}
-	if newUser != nil {
-		newUser.Exec(nId, name)
-		fmt.Println("Record inserted")
-		fmt.Fprint(w, "Records inserted")
+	if model.Insert(w, r) {
+		json.NewEncoder(w).Encode(model.InsertionSuccessMessage)
+	} else {
+		json.NewEncoder(w).Encode(model.ErrorMessage)
 	}
 }
 
 func Update(w http.ResponseWriter, r *http.Request) {
-	nId := r.URL.Query().Get("id")
-	name := r.URL.Query().Get("name")
-	updated, err := database.Db.Prepare("Update users set name=? where id = ?")
-	if err != nil {
-		fmt.Println(err)
-		fmt.Fprint(w, "Params Invalid")
-	}
-
-	if updated != nil {
-		updated.Exec(name, nId)
-		fmt.Println("Record updated")
-		fmt.Fprint(w, "Records updated")
+	if model.Update(w, r) {
+		json.NewEncoder(w).Encode(model.UpdationSuccessMessage)
+	} else {
+		json.NewEncoder(w).Encode(model.ErrorMessage)
 	}
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
-	nId := r.URL.Query().Get("id")
-	deleted, err := database.Db.Prepare("Delete from users where id=?")
-	if err != nil {
-		fmt.Println(err)
-		fmt.Fprint(w, "Params Invalid")
-	}
-
-	if deleted != nil {
-		deleted.Exec(nId)
-		fmt.Println("Record deleted")
-		fmt.Fprint(w, "Record deleted")
+	if model.Delete(w, r) {
+		json.NewEncoder(w).Encode(model.DeletionSuccessMessage)
+	} else {
+		json.NewEncoder(w).Encode(model.ErrorMessage)
 	}
 }
--- mvc-rest/model/users.go
+++ mvc-rest/model/users.go
@@ -1,6 +1,134 @@
 package model
 
+import (
+	"fmt"
+	"mvc-rest/database"
+	"net/http"
+	"strconv"
+)
+
 type User struct {
 	UserId int    `json: "id"`
 	Name   string `json:"name"`
+}
+
+const ErrorMessage string = "Something went wrong"
+const InsertionSuccessMessage string = "Successfully Inserted"
+const UpdationSuccessMessage string = "Successfully Updated"
+const DeletionSuccessMessage string = "Successfully Deleted"
+const InvalidParamsMessage string = "It's not funny to give invalid params"
+
+func AllUsers(w http.ResponseWriter, r *http.Request) interface{} {
+	var users []User
+	pagNo := r.URL.Query().Get("page")
+	pageSize := r.URL.Query().Get("size")
+	pagNoInt, er1 := strconv.Atoi(pagNo)
+	pageSizeInt, er2 := strconv.Atoi(pageSize)
+	if er1 == nil && er2 == nil {
+
+		allUsers, err := database.Db.Prepare("Select id, name From users limit ?, ?")
+		defer allUsers.Close()
+		left := strconv.Itoa((pagNoInt - 1) * pageSizeInt)
+		right := strconv.Itoa(pageSizeInt)
+		if err != nil {
+			fmt.Println(err)
+			return ErrorMessage
+		}
+		fmt.Println(left, right)
+		allUsers1, err := allUsers.Query(left, right)
+		if err != nil {
+			fmt.Println(err)
+			return ErrorMessage
+		}
+		for allUsers1.Next() {
+			var id int
+			var name string
+			err := allUsers1.Scan(&id, &name)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				userData := User{id, name}
+				users = append(users, userData)
+				fmt.Println(id, name)
+			}
+		}
+		return users
+	} else {
+		return InvalidParamsMessage
+	}
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) interface{} {
+	nId := r.URL.Query().Get("id")
+	user, err := database.Db.Query("SELECT id, name FROM users where id = ?", nId)
+	defer user.Close()
+	if err != nil {
+		fmt.Println(err)
+		return ErrorMessage
+	}
+	if user != nil {
+		for user.Next() {
+			var id int
+			var name string
+			err := user.Scan(&id, &name)
+			if err != nil {
+				panic(err.Error())
+			} else {
+				userData := User{id, name}
+				fmt.Println(id, name)
+				return userData
+			}
+		}
+	}
+	return ErrorMessage
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) bool {
+	nId := r.URL.Query().Get("id")
+	name := r.URL.Query().Get("name")
+	newUser, err := database.Db.Prepare("Insert into users(id, name) values(?, ?)")
+	defer newUser.Close()
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprint(w, "Params Invalid")
+		return false
+	}
+	if newUser != nil {
+		newUser.Exec(nId, name)
+		fmt.Println("Record inserted")
+		return true
+	}
+	return false
+}
+func Update(w http.ResponseWriter, r *http.Request) bool {
+	nId := r.URL.Query().Get("id")
+	name := r.URL.Query().Get("name")
+	updated, err := database.Db.Prepare("Update users set name=? where id = ?")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprint(w, "Params Invalid")
+		return false
+	}
+
+	if updated != nil {
+		updated.Exec(name, nId)
+		fmt.Println("Record updated")
+		return true
+	}
+	return false
+}
+func Delete(w http.ResponseWriter, r *http.Request) bool {
+	nId := r.URL.Query().Get("id")
+	deleted, err := database.Db.Prepare("Delete from users where id=?")
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	if deleted != nil {
+		deleted.Exec(nId)
+		fmt.Println("Record deleted")
+		return true
+	}
+	return false
 }
